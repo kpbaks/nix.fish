f247d9d064a938a7d3892bff388ba29788f219fa
kpbaks <kristoffer.pbs@tuta.io>
2025-09-14 20:05:03 +0200

    build: remove `flake-utils` input
    Simplifies output block. The primary output is the
    `homeModules.default` attr which is dependent on a system.

--- flake.nix
+++ flake.nix
@@ -3,47 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter = pkgs.nixfmt-rfc-style;
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            fish
-            mdformat
-            nurl
-          ];
-        };
-      }
-    )
-    // {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          fish
+          mdformat
+          nurl
+        ];
+      };
+
       homeModules.default =
         { config, pkgs, ... }:
         {
-
-          home.packages = with pkgs; [
-            nurl
-            # TODO: ponder or ask someone if this is the "right" way of ensuring `bat` is in $PATH
-            config.programs.bat.package
+          home.packages = [ pkgs.nurl ];
+          programs.bat.enable = true;
+          programs.fish.plugins = [
+            {
+              name = "nix.fish";
+              # TODO: filter list to only be *.fish files
+              src = ./.;
+            }
           ];
-          programs.fish.plugins = {
-            name = "nix.fish";
-            # TODO: filter list to only be *.fish files
-            src = ./.;
-          };
         };
     };
 }
